a38dea8f5ce13a7dd6b5b09f6cd04777d59fc901
IgorNiNo <vis_74@mail.ru>
2020-10-04 21:34:37 +0300

    developLesson5

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -1,6 +1,9 @@
 package lesson5;
 
 import java.util.concurrent.BrokenBarrierException;
+import static lesson5.Main.cdlPreRun;
+import static lesson5.Main.cdlFinished;
+import static lesson5.Main.cb;
 
 public class Car implements Runnable {
 
@@ -8,6 +11,11 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private boolean isLastStage;
+
+    public boolean isLastStage() {
+        return isLastStage;
+    }
 
     public String getName() {
         return name;
@@ -21,6 +29,7 @@ public class Car implements Runnable {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
+        isLastStage = false;
         this.name = "Участник #" + CARS_COUNT;
     }
 
@@ -30,13 +39,27 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdlPreRun.countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        try {
+            cb.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < race.getStages().size(); i++) {
+            if (i == (race.getStages().size() - 1)) {
+                isLastStage = true;
+            }
             race.getStages().get(i).go(this);
         }
+
+        cdlFinished.countDown();
 
     }
 
--- src/lesson5/Road.java
+++ src/lesson5/Road.java
@@ -1,6 +1,11 @@
 package lesson5;
 
+import static lesson5.Main.lock;
+
 public class Road extends Stage {
+
+    private static boolean isWin;
+
     public Road(int length) {
         this.length = length;
         this.description = "Дорога " + length + " метров";
@@ -11,7 +16,13 @@ public class Road extends Stage {
         try {
             System.out.println(c.getName() + " начал этап: " + description);
             Thread.sleep(length / c.getSpeed() * 1000);
+            lock.lock();
             System.out.println(c.getName() + " закончил этап: " + description);
+            if (!isWin && c.isLastStage()) {
+                isWin = true;
+                System.out.println(c.getName() + " - WIN");
+            }
+            lock.unlock();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
